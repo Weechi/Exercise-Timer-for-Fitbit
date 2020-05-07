2692e56738b6b56ac371153e539774fe59c1cc3b
Richard Kraus <kraus@student.tugraz.at>
2020-05-07 19:29:14 +0200

    very sketchy code

--- app/index.js
+++ app/index.js
@@ -1,1 +1,42 @@
-console.log('Hello world!');
+import document from "document";
+import clock from "clock";
+import TimR from "timer.js";
+
+
+clock.granularity = "minutes";
+clock.ontick = (evt) => {
+   console.log(evt.date.toTimeString());
+  
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+// Regex
+let re = /ab+c/;
+
+
+
+let woo = document.getElementById("arc_time");
+//let NONO = woo.style.getPropertyValue("start-angle");
+
+
+let demotext = document.getElementById("demotext");
+//demotext.text = "NONO";
+
+import document from "document";
+import * as messaging from "messaging";
+
+var current_time_ms = 500;
+
+step();
+
+
+
+function Countdown(time) {
+
+  //let arc = document.getElementById("arc_time");
+  //int start = parseInt(convert.arc.style.start-angle);
+  
+}
---
+++ app/timer.js
@@ -0,0 +1,16 @@
+class Timer{
+    constructor(settings){
+      //load settings form setting storage
+      
+      
+    }
+    
+    function step(){
+      
+       document.getElementById('xxx').setAttribute('width', 'somevalue')
+      
+    }
+    //woo.style.start-angle = 30; //woo.getPropertyValue("start-angle" + 30);
+    woo.style.fill = "red"; // ToString(parseInt())
+    
+  }
